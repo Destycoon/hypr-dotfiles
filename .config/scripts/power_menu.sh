9ade3f4782f09b0ef5588dc9708e4ad9a814f8a1
Destycoon <destycoon@gmail.com>
2025-08-08 01:05:47 +0200

    changement mineurs

--- .config/scripts/power_menu.sh
+++ .config/scripts/power_menu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 choice=$(printf "⏻ Power Off\n Reboot\n Suspend\n Hibernate\n Lock" | \
-  wofi --dmenu --cache-file /dev/null --width 300 --height 250 --hide-scroll --prompt "Power Menu")
+  wofi --dmenu --cache-file /dev/null --width 300 --height 270 --hide-scroll --prompt "Power Menu")
 
 case "$choice" in
   "⏻ Power Off") systemctl poweroff ;;
