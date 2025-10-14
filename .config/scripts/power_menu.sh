be95c8fc117a0ed025c6b68af530547536dce380
Destycoon <destycoon@gmail.com>
2025-10-14 14:22:40 +0200

    change path

--- .config/scripts/power_menu.sh
+++ .config/scripts/power_menu.sh
@@ -3,5 +3,5 @@
 if pgrep -f "qs.*PowerMenu.qml" > /dev/null; then
     pkill -f "qs.*PowerMenu.qml"
 else
-    qs -p "/home/destycoon/.config/quickshell/bar/modules/power_menu/PowerMenu.qml"
+    qs -p "/home/destycoon/.config/quickshell/modules/bar/barModules/PowerMenu.qml"
 fi
